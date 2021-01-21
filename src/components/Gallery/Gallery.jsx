513ff156e33bcc133ce88f365f4d9eee5ddaf498
Ahti <ahti.ruusuvuori@abako.fi>
2021-01-21 14:32:07 +0200

    [MOD] styled gallery

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { observer } from "mobx-react-lite";
+import styled from "styled-components";
 import PhotoStore from "../../stores/PhotoStore";
 import Thumbnail from "./Thumbnail/Thumbnail";
+
+const StyledGalleryContainer = styled.div`
+	display: flex;
+	justify-content: center;
+	flex-wrap: wrap;
+`;
 
 const photoStore = new PhotoStore();
 
 const App = observer(() => {
 	return (
-		<>
+		<StyledGalleryContainer>
 			{photoStore.photos.map(photo => (
 				<Thumbnail
 					key={photo.id}
@@ -15,7 +22,7 @@ const App = observer(() => {
 					title={photo.title}
 				/>
 			))}
-		</>
+		</StyledGalleryContainer>
 	);
 });
 
