2f1175e89cbb550e5ae38faee920c722557dbb13
Ahti <ahti.ruusuvuori@abako.fi>
2021-01-21 12:30:35 +0200

    [MOD] fix indentations

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -6,17 +6,17 @@ import Thumbnail from "./Thumbnail/Thumbnail";
 const photoStore = new PhotoStore();
 
 const App = observer(() => {
-  return (
-    <>
-      {photoStore.photos.map(photo => (
-		<Thumbnail
-			key={photo.id}
-			thumbnailUrl={photo.thumbnailUrl}
-			title={photo.title}
-		/>
-      ))}
-    </>
-  );
+	return (
+		<>
+			{photoStore.photos.map(photo => (
+				<Thumbnail
+					key={photo.id}
+					thumbnailUrl={photo.thumbnailUrl}
+					title={photo.title}
+				/>
+			))}
+		</>
+	);
 });
 
 export default App;
