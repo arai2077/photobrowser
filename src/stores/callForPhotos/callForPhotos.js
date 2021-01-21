5c21d715e935a45c0dbb25bd3a75810322de5d4c
Ahti <ahti.ruusuvuori@abako.fi>
2021-01-21 12:30:23 +0200

    [MOD] remove debug

--- src/stores/callForPhotos/callForPhotos.js
+++ src/stores/callForPhotos/callForPhotos.js
@@ -1,5 +1,4 @@
 const callForPhotos = async () => {
-	console.log("calling photo api")
 	const response = await fetch("http://jsonplaceholder.typicode.com/photos");
 	return await response.json();
 };
