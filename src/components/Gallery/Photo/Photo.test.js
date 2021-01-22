ae0bd368909bb2bf851d288305099792e39f8e7e
Ahti <ahti.ruusuvuori@abako.fi>
2021-01-22 13:33:50 +0200

    [MOD] photo render test

--- src/components/Gallery/Photo/Photo.test.js
+++ src/components/Gallery/Photo/Photo.test.js
@@ -1,10 +1,17 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Photo from "./Photo";
 
 describe("Photo", () => {
   it("renders", () => {
-    const component = shallow(<Photo />);
+    const component = mount(
+      <Router>
+        <Route path="Some route">
+          <Photo />
+        </Route>
+      </Router>
+    );
 
     expect(component).toMatchSnapshot();
   });
